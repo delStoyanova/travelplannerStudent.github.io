f398d05ae94d1fedf04edbe209e0400e601fb505
travelplannerStudent <d.stoyanova@student.tue.nl>
2016-12-04 20:00:40 +0100

    forth commit

--- js/share.js
+++ js/share.js
@@ -18,24 +18,56 @@ $(document).ready(function () {
              a parameter for the functions
              */
             $('.list').append('<p class="posted" >' + text + '</p> <span id="countedlikes' + count + '">0 Likes</span>' +
-                ' <img  onclick="AddLikes(' + count + ')" id="like' + count + '" src="img/like.jpg">' +
-                ' <img  onclick="RemoveLikes(' + count + ')" id="dislike' + count + '" src="img/dislike.jpg"> ');
+                ' <img class="buttonlikes"  onclick="AddLikes(' + count + ')" id="like' + count + '" src="img/like.jpg">' +
+                ' <img class="buttonlikes"  onclick="RemoveLikes(' + count + ')" id="dislike' + count + '" src="img/dislike.jpg">' +
+                '<hr class="featurette-divider"> ');
 
             count++;
             document.getElementById("share").value = "";
 
         }
+       /* $('.buttonlikes').click(function () {
+            $(this).addClass('active');
+        });*/
+        $(".buttonlikes").mousedown(function () {
+
+
+            var top = parseInt($(this).css("margin-bottom"));
+
+            var left= parseInt($(this).css("margin-left"));
+
+            top = top + 3;
+
+            left = left + 3;
+
+            $(this).css("margin-bottom", top + "px").css("margin-left", left + "px");
+
+
+        }).mouseup(function () {
+
+            var top = parseInt($(this).css("margin-top"));
+
+            var left = parseInt($(this).css("margin-left"));
+
+            top = top - 3;
+
+            left = left - 3;
+
+            $(this).css("margin-bottom", top + "px").css("margin-left", left + "px");
+
+        });
     });
 
 });
 /*the functions for incrementing and decrementing likes are similar, they use a parameter send by the button click
- for finding the corresponding span tag; the content is put in variable then the first two digits are turned into integer
+ for finding the corresponding span tag; the content is put in variable then it is turned into integer
  that integer is changed according to which button is pressed
  */
 function AddLikes(c) {
 
     var n = document.getElementById('countedlikes' + c + '').innerHTML;
-    var likes = parseInt(n.substr(0, 2));
+    //var likes = parseInt(n.substr(0, 4));
+    var likes = parseInt(n);
     document.getElementById('countedlikes' + c + '').innerHTML = "";
     likes = likes + 1;
     document.getElementById('countedlikes' + c + '').innerHTML = likes + ' Likes';
@@ -44,7 +76,7 @@ function AddLikes(c) {
 }
 function RemoveLikes(c) {
     var n = document.getElementById('countedlikes' + c + '').innerHTML;
-    var likes = parseInt(n.substr(0, 2));
+    var likes = parseInt(n);
     document.getElementById('countedlikes' + c + '').innerHTML = "";
     likes = likes - 1;
     if (likes <= 0) {
